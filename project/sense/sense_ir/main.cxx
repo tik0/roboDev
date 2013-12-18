020727ee0d56070c431b297a9580a61895c5d289
Timo Korthals <tkorthals@cit-ec.uni-bielefeld.de>
2013-12-18 17:49:52 +0100

    Added messages and cleanup

--- project/sense/sense_ir/main.cxx
+++ project/sense/sense_ir/main.cxx
@@ -6,26 +6,25 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include "infraRed.h"
+#define INFO_MSG_
+#define DEBUG_MSG_
+#define SUCCESS_MSG_
+#define WARNING_MSG_
+#define ERROR_MSG_
+#include <MSG.h>
 
-//#include <stdio.h>
+#include <FLAGS.h>
+
+#include "infraRed.h"
 #include <bebot.h>
 #include <iostream>
 
-//using namespace std;
-
-#define DEBUG
-
-//#ifdef DEBUG
-//#define DEBUG_MSG(str) { std::cout << argv[0] << ": str " << std::endl; }
-//#else
-//#define DEBUG_MSG(str) {}
-//#endif
-
+using namespace std;
 
 int main (int argc, const char **argv){
 
-//	DEBUG_MSG(Start sending ir information in )
+	INFO_MSG("Start sending ir information in ")
+
 //	std::cout << "[sense IR] Start sending ir information in '/sense/ir'" << std::endl;
 
 	//Start up with initializing the robot
@@ -34,18 +33,13 @@ int main (int argc, const char **argv){
 
 
 	SensorInformer::InfraRed myIR(&BEBOT);
-	std::cout << "test" << std::endl;
-
-	std::cout << "test" << std::endl;
 
 	// This loop needs to be parameterized regarding the update-frequency
 	for(;;) {
-		myIR.sendIRData();
+//		myIR.sendIRData();
+		ERROR_MSG("This is some variable")
 		sleep(1);
 	}
-
-//	DEBUG_MSG(Stop sending ir information)
-//	std::cout << "[HardwareInterface] Ending HardwareInterface" << std::endl;
 
 	return EXIT_SUCCESS;
 }
